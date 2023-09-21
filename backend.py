8b18c13b640c83d8d22b43896d21e46feba4adab
Onion22Bolt <99860769+Onion22Bolt@users.noreply.github.com>
2023-09-21 19:05:17 +0600

    Update and rename app.py to backend.py

--- backend.py
+++ backend.py
@@ -4,10 +4,10 @@ import socket
 app = Flask(__name__)
 
 @app.route('/')
-def get_info():
+def get_instance_info():
     instance_id = socket.gethostname()
-    public_ip = socket.gethostbyname(socket.gethostname())
-    return jsonify({'instanceId': instance_id, 'publicIp': public_ip})
+    public_ip = socket.gethostbyname(instance_id)
+    return jsonify(instanceId=instance_id, publicIp=public_ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
