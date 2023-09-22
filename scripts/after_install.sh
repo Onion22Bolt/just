76ce960dede5a7175d8a7a9f0fffc923df4352cd
Onion22Bolt <99860769+Onion22Bolt@users.noreply.github.com>
2023-09-23 01:35:45 +0600

    3Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/
-sudo chown -R ec2-user:ec2-user backend.py
-sudo chown -R ec2-user:ec2-user index.html
-sudo chown -R ec2-user:ec2-user requirements.txt
+sudo chown -R ec2-user:ec2-user project
